18b3d8374a21547110c1967e45b34285bd6ff301
DenisDuev <denis_duev@abv.bg>
2016-04-18 22:07:56 +0300

    Correct tank sizes and collision

--- src/constants/Constants.java
+++ src/constants/Constants.java
@@ -7,7 +7,7 @@ public class Constants {
     public static final int BOARD_PADDING = 200;
 
     public static final int MATRIX_CELL_SIZE = 30;
-    public static final int TANK_SIZE = 28;
+    public static final int TANK_SIZE = 26;
 
     public static final int MATRIX_ROWS = 20;
     public static final int MATRIX_COLS = 20;
