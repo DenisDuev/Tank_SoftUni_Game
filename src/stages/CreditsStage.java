124acbc6a1b064327e8cc96078144d0db6e86b53
Martin Lachev <martinlachev1@gmail.com>
2016-04-21 12:03:54 +0300

    Add CreditsStage with Fonts

--- src/stages/CreditsStage.java
+++ src/stages/CreditsStage.java
@@ -5,7 +5,8 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.VBox;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
@@ -28,18 +29,31 @@ public class CreditsStage {
     public void show(){
 
 
-        Text creditsDenis = new Text("Denis Duev - from Krupnik, graduated from PMG Blagoevgrad with a specialty in mathematics and informatics \n works as a creator of online video lessons on mathematics for the internet site SuperUrok.com. has experience \n with C# and Java, has certificates for accomplishing courses C# Basics, Data structures and Java Fundamentals \n at SoftUni, student of SoftUni and Sofia University");
-        Text creditsMarto = new Text("Martin Lachev - from Sofia, unfortunatelly curently studing in UNWE and fortunatelly in Softuni  ");
-
+        Text creditsDenis = new Text("Denis Duev - from Krupnik, graduated from PMG Blagoevgrad with\na specialty in mathematics and informatics works as a creator of online\nvideo lessons on mathematics for the internet site SuperUrok.com. has\nexperience with C# and Java, has certificates for accomplishing courses\nC# Basics, Data structures and Java Fundamentalsat SoftUni,\nstudentof SoftUni and Sofia University");
+        Text creditsMarto = new Text("Martin Lachev - from Sofia, unfortunatelly curently studing in UNWE and\n fortunatelly in Softuni  ");
+        creditsDenis.setFont(Font.font(18));
+        creditsMarto.setFont(Font.font(18));
 
         Button backButton = new MenuButton("Back");
         backButton.setOnMouseClicked(e -> {
             stage.setScene(mainMenuScene);
         });
 
+        Background background = new Background(
+                new BackgroundImage(
+                        new Image("resources/menu_background.png",
+                                Constants.WINDOWS_WIDTH + Constants.PADDING,
+                                Constants.WINDOWS_HEIGHT + Constants.PADDING,
+                                false, false),
+                        BackgroundRepeat.NO_REPEAT,
+                        BackgroundRepeat.NO_REPEAT,
+                        BackgroundPosition.CENTER,
+                        BackgroundSize.DEFAULT));
+
         VBox vBox = new VBox(20);
         vBox.setAlignment(Pos.CENTER);
         vBox.getChildren().addAll(creditsDenis,creditsMarto, backButton);
+        vBox.setBackground(background);
 
         Scene scene = new Scene(vBox, Constants.WINDOWS_WIDTH, Constants.WINDOWS_HEIGHT, Color.BLACK);
         this.stage.setScene(scene);
